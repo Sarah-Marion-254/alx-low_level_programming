42128c44c24315b2262b514db1387cae4359257f
Sarah-Marion-254 <sarahmarion997@gmail.com>
2023-03-20 09:59:01 -0700

    uppercase and lowercase

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This marks the start of code
+ *
+ * Return: Returns 0 if Success
+ */
+int main(void)
+{
+	int  m;
+
+
+	for (m = 0; m < 10; m++)
+	{
+		printf("%d", m);
+	}
+	printf("\n");
+	return (0);
+}
