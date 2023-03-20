c2e83a824aecdd358dc166c3473d2d95943e5018
Sarah-Marion-254 <sarahmarion997@gmail.com>
2023-03-20 10:06:43 -0700

    uppercase and lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This marks the start of code
+ *
+ * Return: Returns 0 if success
+ */
+int main(void)
+{
+	char m;
+
+
+	for (m = 'z'; m >= 'a'; m--)
+	{
+		putchar (m);
+	}
+	putchar('\n');
+	return (0);
+}
