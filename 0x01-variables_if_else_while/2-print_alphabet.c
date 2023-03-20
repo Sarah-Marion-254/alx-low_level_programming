7eb448a24f7cbb694e2c187e563c08a0c3e88594
Sarah-Marion-254 <sarahmarion997@gmail.com>
2023-03-20 09:49:05 -0700

    uppercase and lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This marks the start of code
+ *
+ * Return: gives a success message 0
+ */
+int main(void)
+{
+	char m;
+
+
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		putchar (m);
+	}
+	putchar('\n');
+	return (0);
+}
+
